1a6625f045fa434a77cfa0838c8370899eae7a2b
Mason <mwitcraft11@gmail.com>
2018-03-13 18:18:22 -0500

    SortedArray's insert function now works using arrays

--- SortedArray.cpp
+++ SortedArray.cpp
@@ -34,55 +34,109 @@ template<class DT>
 SortedArray<DT>::SortedArray(int size){
     arraySize = size;
     elements = new DT[size];
+    numElements = 0;
 }
 
 template<class DT>
 int SortedArray<DT>::insert(DT& newElement){
-    DT* tempElements = new DT[arraySize];
-    int index;
+    elements[numElements] = newElement;
+    ++numElements;
+
+
+    for(int i = 1; i < numElements; ++i){
+        int cur = elements[i];
+        int j = i - 1;
+        while((j >= 0) && (elements[j] > cur)){
+            elements[j + 1] = elements[j];
+            --j;
+        }
+        elements[j+1] = cur;
+    }
+
     int low = 0;
-    int high = arraySize;
+    int high = numElements;
     while(low <= high){
         int mid = (low + high)/2;
-        if(elements[mid] == NULL){
+        if(elements[mid] == newElement){
+            return mid;
+        }
+        if(elements[mid] > newElement){
             high = mid - 1;
-            if(mid == 0 && elements[mid] == NULL){
-                index = 0;
-                low = high + 1;
-            }
-        } else if(elements[mid] < newElement && (elements[mid + 1] > newElement || elements[mid + 1] == NULL)){
-            if(elements[mid + 1] == NULL){
-                index = mid + 1;
-            } else {
-                index = mid;
-            }
-            low = high + 1;
-        } else if(elements[mid] < newElement){
+        }
+        if(elements[mid] < newElement){
             low = mid + 1;
-        } else if(elements[mid] > newElement && (elements[mid - 1] < newElement || elements[mid - 1] == NULL)){
-            index = mid;
-            low = high + 1;
-        } else if(elements[mid] > newElement){
-            high = mid - 1;
         }
     }
+    
 
-    // cout << "Index: " << index;
 
-    for(int i = 0; i < arraySize; ++i){
-        if(i < index){
-            tempElements[i] = elements[i];
-        }
-        if(i == index){
-            tempElements[i] = newElement;
-        }
-        if(i > index){
-            tempElements[i] = elements[i - 1]; 
-        }
-    }
-    elements = tempElements;
 
-    return index;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // DT* tempElements = new DT[arraySize];
+    // int index;
+    // int low = 0;
+    // int high = arraySize;
+    // while(low <= high){
+    //     int mid = (low + high)/2;
+    //     if(elements[mid] == NULL){
+    //         high = mid - 1;
+    //         if(mid == 0 && elements[mid] == NULL){
+    //             index = 0;
+    //             low = high + 1;
+    //         }
+    //     } else if(elements[mid] < newElement && (elements[mid + 1] > newElement || elements[mid + 1] == NULL)){
+    //         if(elements[mid + 1] == NULL){
+    //             index = mid + 1;
+    //         } else {
+    //             index = mid;
+    //         }
+    //         low = high + 1;
+    //     } else if(elements[mid] < newElement){
+    //         low = mid + 1;
+    //     } else if(elements[mid] > newElement && (elements[mid - 1] < newElement || elements[mid - 1] == NULL)){
+    //         index = mid;
+    //         low = high + 1;
+    //     } else if(elements[mid] > newElement){
+    //         high = mid - 1;
+    //     }
+    // }
+
+    // // cout << "Index: " << index;
+
+    // for(int i = 0; i < arraySize; ++i){
+    //     if(i < index){
+    //         tempElements[i] = elements[i];
+    //     }
+    //     if(i == index){
+    //         tempElements[i] = newElement;
+    //     }
+    //     if(i > index){
+    //         tempElements[i] = elements[i - 1]; 
+    //     }
+    // }
+    // elements = tempElements;
+
+    // return index;
 }
 
 template<class DT>
@@ -110,33 +164,41 @@ int SortedArray<DT>::remove(DT& oldElement){
 
 template<class DT>
 void SortedArray<DT>::print(){
-    for(int i = 0; i < arraySize; ++i){
-        if(elements[i] != NULL){
-            cout << "{" << i << "," << elements[i] << "}" << endl;
-        } else {
-            cout << "At least one null" << endl;
-        }
+    // for(int i = 0; i < arraySize; ++i){
+    //     if(elements[i] != NULL){
+    //         cout << "{" << i << "," << elements[i] << "}" << endl;
+    //     } else {
+    //         cout << "At least one null" << endl;
+    //     }
+    // }
+    for(int i = 0; i < numElements; ++i){
+        cout << elements[i] << " ";
     }
+    cout << endl;
 }
 
 int main(){
     SortedArray<int>* newArray = new SortedArray<int>(5);
 
-    int val = 1;
+    int val = 6;
     int temp = newArray->insert(val);
-    cout << temp << endl;
-
-    val = 6;
-    temp = newArray->insert(val);
-    cout << temp << endl;
+    cout << "*" << temp << endl;
 
     val = 4;
     temp = newArray->insert(val);
-    cout << temp << endl;
+    cout << "*" << temp << endl;    
 
-    val = 2;
+    val = 1;
     temp = newArray->insert(val);
-    cout << temp << endl;
+    cout << "*" << temp << endl;        
+
+    val = 5;
+    temp = newArray->insert(val);
+    cout << "*" << temp << endl;
+
+    val = 7;
+    temp = newArray->insert(val);
+    cout << "*" << temp << endl;            
 
     newArray->print();
 
